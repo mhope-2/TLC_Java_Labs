de6d787f4601cfb927d3d7b64607b1f14cc2a160
Michael Hope <hopemichael720@gmail.com>
2021-10-07 07:36:35 +0000

    feat: added Streams lab

---
+++ Streams/ReadFile.java
@@ -0,0 +1,25 @@
+package Streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        Path path = Paths.get(System.getProperty("user.dir")+"\\Streams\\Trades.txt");
+
+        try{
+            List<String> contents = Files.readAllLines(path);
+
+            // read from the stream
+            for (Object content : contents){
+                System.out.println(content);
+            }
+            System.out.println();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
