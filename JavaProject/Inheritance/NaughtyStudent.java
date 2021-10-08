b7c43a5a994b2a25783f270a899bedbb34ea9474
Michael Hope <hopemichael720@gmail.com>
2021-10-08 15:08:58 +0000

    feat: added java project

---
+++ JavaProject/Inheritance/NaughtyStudent.java
@@ -0,0 +1,2 @@
+package JavaProject.Inheritance;public class NaughtyStudent {
+}
---
+++ JavaProject/Inheritance/NaughtyStudentTest.java
@@ -0,0 +1,8 @@
+package JavaProject.BasicJava;
+
+import org.junit.Test;
+
+public class NaughtyStudentTest {
+    @Test
+    public void testInstanceOfStudent
+}
